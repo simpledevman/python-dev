8d2472cb7aef6166d88635353b7fe016cf007f96
simpledevman <dev.heeroe@gmail.com>
2022-05-25 17:32:01 +0800

    level 1

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ semua sintaksis dasar bahasa pemrograman terdiri dari:
 3. perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
 
-print("ibu berkata")
+print("ibu berkata, 'pergi ke toko' ")
+print("budi menjawab, 'baik , apa yang harus saya lakukan di toko?'")
+print("ibu menjawab, 'beli satu botol susu, dan jika ada telur beli 6'")
+print("maka budi berkangkat ke toko")
+print("dan mulai berbelanja")
