7be19e025111507c12536ae4ceabb67315012f45
simpledevman <dev.heeroe@gmail.com>
2022-05-27 08:36:29 +0800

    perulangan dengan while dengan jumlah tak terbatas dan variabe pemberhenti

---
+++ dasar_sintaksis_perulangan_baca_buku_dengan_while_undefined_count.py
@@ -0,0 +1,20 @@
+"""
+program perulangan membaca buku dengan while
+"""
+jumlah_buku = 10
+print("ibu perintah, baca semua bukumu")
+total_jumlah_baca = 0
+
+jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
+print(f"jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}")
+
+while total_jumlah_baca < jumlah_buku * 2:
+    total_jumlah_baca = total_jumlah_baca + 1
+    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9:
+        print(f"buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami +1} belum faham")
+    else:
+        jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
+        print(f"buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami")
+
+
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca_dan_dipahami}")
